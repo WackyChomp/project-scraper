75f5bcc6ea2b68936dc6f83087566888f0b6c24a
Wilson Chan <wilsonchan54@gmail.com>
2021-03-19 22:31:05 -0400

    Creating soup variable to call BeautifulSoup

--- Scraper.py
+++ Scraper.py
@@ -2,7 +2,10 @@
 from bs4 import BeautifulSoup
 import requests
 
+#Google images of monkeys
 url = "https://www.google.com/search?q=monkey&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjp182qrrvvAhXMmeAKHXYwBU4Q_AUoAXoECAIQAw"
 
 response = requests.get(url)         #requesting permission to take contents from url
+
+soup = BeautifulSoup(response.content, "html.parser")     #instantiate web scraper / parser that iterates over HTML
 
