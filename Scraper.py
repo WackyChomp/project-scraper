323f8a46f59dfb81ceb93be4f04a9259ebd56c4d
Wilson Chan <wilsonchan54@gmail.com>
2021-03-19 22:59:59 -0400

    Adding attribute to parameter in image variable / importing urllib.request

--- Scraper.py
+++ Scraper.py
@@ -1,6 +1,7 @@
 
 from bs4 import BeautifulSoup
 import requests
+import urllib.request
 
 #Google images of monkeys
 url = "https://www.google.com/search?q=monkey&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjp182qrrvvAhXMmeAKHXYwBU4Q_AUoAXoECAIQAw"
@@ -9,7 +10,8 @@ response = requests.get(url)         #requesting permission to take contents fro
 
 soup = BeautifulSoup(response.content, "html.parser")     #instantiate web scraper / parser that iterates over HTML
 
-image = soup.find_all("img")          #extract tags inside the string and stores them inside a list
- 
+image = soup.find_all("img" , attrs = {"class" : "rg_i Q4LulWd"})    #extract tags inside the string and stores them inside a list as a dictionary
+
+
 
 #print(soup.prettify())         #prints out HTML file from URL
