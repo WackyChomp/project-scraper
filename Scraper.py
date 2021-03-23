39581a087a09b526ad2bd9f4e861daae910b5b15
Wilson Chan <wilsonchan54@gmail.com>
2021-03-22 23:32:22 -0400

    Adding number variable to name and keep track of the image scraped / create for loop to scrape images

--- Scraper.py
+++ Scraper.py
@@ -1,7 +1,7 @@
 
 from bs4 import BeautifulSoup
 import requests
-import urllib.request
+import urllib.request         #download from the sources attribute link
 
 #Google images of monkeys
 url = "https://www.google.com/search?q=monkey&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjp182qrrvvAhXMmeAKHXYwBU4Q_AUoAXoECAIQAw"
@@ -10,8 +10,14 @@ response = requests.get(url)         #requesting permission to take contents fro
 
 soup = BeautifulSoup(response.content, "html.parser")     #instantiate web scraper / parser that iterates over HTML
 
-image = soup.find_all("img" , attrs = {"class" : "rg_i Q4LulWd"})    #extract tags inside the string and stores them inside a list as a dictionary
+images = soup.find_all("img" , attrs = {"class" : "rg_i Q4LulWd"})    #extract tags inside the string and stores them inside a list as a dictionary
 
+number = 0
 
+for image in images:
+    image_src = image["src"]       #grab source URL for image inside the source attribute of the tag 
+    urllib.request.urlretrieve(image_src , str(number))      #2nd parameter is the name of the file. Using number as an example 
+    print(image_src)
+    number += 1
 
 #print(soup.prettify())         #prints out HTML file from URL
